3493ed373f8c3c304ff170c32f492fcb17bd093d
brightdevs <109676581+brightdevs@users.noreply.github.com>
2022-09-08 11:23:57 -0300

    proc file created

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import App from './app';
 import PostsController from './controllers/posts.controller';
 import dotenv from 'dotenv';
 dotenv.config();
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 const app = new App([new PostsController()], PORT as unknown as number);
 
 app.listen();
