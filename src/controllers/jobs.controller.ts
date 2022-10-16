7d1db40d9cc69c2f1248a5f95f7391c81e395707
Edgar <38365682+e-rojas@users.noreply.github.com>
2022-10-15 23:22:58 -0300

    jobs controller created

---
+++ src/controllers/jobs.controller.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import Job from '../interfaces/job.interface';
+import jobModel from '../models/job.model';
+
+class JobsController {
+  public path = '/jobs';
+  public router = express.Router();
+  private job = jobModel;
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(this.path, this.getAllJobs);
+    this.router.get(`${this.path}/:id`, this.getJobById);
+  }
+
+  private getAllJobs = (req: express.Request, res: express.Response) => {
+    this.job
+      .find()
+      .then((jobs) => {
+        res.send(jobs);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: 'Error during getting all jobs',
+          error: err,
+        });
+      });
+  };
+
+  private getJobById = (req: express.Request, res: express.Response) => {
+    const id = req.params.id;
+    this.job
+      .findById(id)
+      .then((job) => {
+        if (job) {
+          res.send(job);
+        } else {
+          res.status(404).json({
+            message: 'Job not found',
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: 'Error during getting job',
+          error: err,
+        });
+      });
+  };
+}
+
+export default JobsController;
